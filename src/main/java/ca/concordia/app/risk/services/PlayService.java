0e7cf0315f299be1e569de3bffebf232171c8608
Pinkal-Shah <pinkalshah270594@gmail.com>
2019-10-03 16:44:50 -0400

    Added Service Method for Fortify

--- src/main/java/ca/concordia/app/risk/services/PlayService.java
+++ src/main/java/ca/concordia/app/risk/services/PlayService.java
@@ -1,8 +1,12 @@
 package ca.concordia.app.risk.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import ca.concordia.app.risk.model.cache.RunningGame;
 import ca.concordia.app.risk.model.dao.PlayerDaoImpl;
 import ca.concordia.app.risk.model.xmlbeans.PlayerModel;
+import ca.concordia.app.risk.model.xmlbeans.CountryModel;
+import ca.concordia.app.risk.shell.ShellHelper;
+import ca.concordia.app.risk.exceptions.RiskGameRuntimeException;
 
 /**
  * 
@@ -11,14 +15,51 @@ import ca.concordia.app.risk.model.xmlbeans.PlayerModel;
  */
 public class PlayService {
 
-	/**
-	 * 
-	 * @param attacker
-	 * @throws Exception
-	 */
-	public void attack(String attacker) {
-		PlayerDaoImpl playerDao = new PlayerDaoImpl();
-		PlayerModel playerModel = playerDao.findByName(RunningGame.getInstance(), attacker);
-		playerModel.setColor("White");
-	}
+  @Autowired
+  ShellHelper shellHelper;
+
+  /**
+   * 
+   * @param attacker
+   * @throws Exception
+   */
+  public void attack(String attacker) throws Exception {
+    PlayerDaoImpl playerDao = new PlayerDaoImpl();
+    PlayerModel playerModel = playerDao.findByName(RunningGame.getInstance(), attacker);
+    playerModel.setColor("White");
+  }
+
+  /**
+   * 
+   * @param fromCountry
+   * @param toCountry
+   * @param numberOfArmies
+   */
+  public String fortify(String fromCountry, String toCountry, int numberOfArmies) {
+    CountryModel fromCountryModel = RunningGame.getInstance().getCountries().getList().stream()
+        .filter((c -> (c.getName().equals(fromCountry)))).findAny().orElse(null);
+
+    if (fromCountryModel != null) {
+      fromCountryModel.setNumberOfArmies(fromCountryModel.getNumberOfArmies() + numberOfArmies);
+    } else {
+      throw new RiskGameRuntimeException("From Country does not exist");
+    }
+
+    CountryModel toCountryModel = RunningGame.getInstance().getCountries().getList().stream()
+        .filter((c -> (c.getName().equals(toCountry)))).findAny().orElse(null);
+
+    if (toCountryModel != null) {
+      if (toCountryModel.getNumberOfArmies() < numberOfArmies
+          || toCountryModel.getNumberOfArmies() - numberOfArmies < 1) {
+        throw new RiskGameRuntimeException("Cannot fortify since the no of armies exceed the allowed limit");
+      } else {
+        toCountryModel.setNumberOfArmies(toCountryModel.getNumberOfArmies() - numberOfArmies);
+      }
+    } else {
+      throw new RiskGameRuntimeException("To Country does not exist");
+    }
+
+    return shellHelper.getSuccessMessage("Fortification Done Successfully");
+  }
+
 }
