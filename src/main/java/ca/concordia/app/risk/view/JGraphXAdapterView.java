d705de0213dc97afa7c6ac22b167f91823e3774a
Nasim Adabi <nasim.adabi@gmail.com>
2019-10-03 18:40:11 -0400

    Some changes of Graph View

--- src/main/java/ca/concordia/app/risk/view/JGraphXAdapterView.java
+++ src/main/java/ca/concordia/app/risk/view/JGraphXAdapterView.java
@@ -1,5 +1,6 @@
 package ca.concordia.app.risk.view;
 
+import java.awt.Color;
 import java.awt.Dimension;
 
 import javax.swing.JApplet;
@@ -10,9 +11,14 @@ import org.jgrapht.ext.JGraphXAdapter;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.DefaultListenableGraph;
 
+
 //@example:full:begin
 import com.mxgraph.layout.mxCircleLayout;
+import com.mxgraph.model.mxCell;
+import com.mxgraph.model.mxGeometry;
 import com.mxgraph.swing.mxGraphComponent;
+import com.mxgraph.util.mxConstants;
+import com.mxgraph.view.mxStylesheet;
 
 import ca.concordia.app.risk.model.cache.RunningGame;
 
@@ -24,7 +30,7 @@ import ca.concordia.app.risk.model.cache.RunningGame;
 public class JGraphXAdapterView extends JApplet {
 	private static final long serialVersionUID = 2202072534703043194L;
 
-	private static final Dimension DEFAULT_SIZE = new Dimension(530, 320);
+	private static final Dimension DEFAULT_SIZE = new Dimension(530, 420);
 
 	private JGraphXAdapter<String, DefaultEdge> jgxAdapter;
 
@@ -36,26 +42,79 @@ public class JGraphXAdapterView extends JApplet {
 	 */
 	public static void main(String[] args) {
 		JGraphXAdapterView applet = new JGraphXAdapterView();
+		
 		applet.init();
+		
 		JFrame frame = new JFrame();
 		frame.getContentPane().add(applet);
 		frame.setTitle(".:: RiskGame - Show Current Game Map");
 		frame.pack();
 		frame.setVisible(true);
-	}
+		frame.setLocationByPlatform(true);
+		
+		//Size of the frame
+		frame.setPreferredSize(DEFAULT_SIZE);
+		
+		//Shows window in the middle of the screen
+		frame.setLocationRelativeTo(null);
 
+		
+		
+	}
+	
 	@Override
 	public void init() {
 		ListenableGraph<String, DefaultEdge> g = new DefaultListenableGraph<>(RunningGame.getInstance().getGraph());
-
+		
 		// create a visualization using JGraph, via an adapter
 		jgxAdapter = new JGraphXAdapter<>(g);
+
+		jgxAdapter.selectAll();
+        Object[] cells = jgxAdapter.getSelectionCells(); //here you have all cells
+
+
+        jgxAdapter.clearSelection(); 
+        
 		jgxAdapter.setLabelsVisible(true);
 		
-		setPreferredSize(DEFAULT_SIZE);
+		jgxAdapter.setAutoSizeCells(true);
+		jgxAdapter.alignCells(mxConstants.ALIGN_CENTER);
+		
+		mxStylesheet stylesheet = jgxAdapter.getStylesheet();
+		
+		
+		//Make vertices style -> rounded instead of hard rectangle
+		stylesheet.getDefaultVertexStyle().put(mxConstants.STYLE_ROUNDED, true);
+		//stylesheet.getDefaultVertexStyle().put(mxConstants.STYLE_SHAPE, mxConstants.SHAPE_ELLIPSE);
+		//stylesheet.getDefaultVertexStyle().put(mxConstants.STYLE_FONTCOLOR, Color.WHITE);
+		
+		//Coloring the Vertices
+		stylesheet.getDefaultVertexStyle().put(mxConstants.STYLE_FILLCOLOR, Color.YELLOW.getRGB());
+		//Let's do add shadow
+		stylesheet.getDefaultVertexStyle().put(mxConstants.STYLE_GRADIENTCOLOR, "#A9C4EB");
+		stylesheet.getDefaultVertexStyle().put(mxConstants.STYLE_SHADOW, true);
+		
+
+
+		
+		jgxAdapter.setStylesheet(stylesheet);
+		jgxAdapter.getStylesheet().getDefaultEdgeStyle().put(mxConstants.STYLE_DASHED, "1");
+		
+
+		//setPreferredSize(DEFAULT_SIZE);
 		mxGraphComponent component = new mxGraphComponent(jgxAdapter);
-		component.setConnectable(false);
+	 
+		
+		//Perevents drawing edges from user side
 		component.getGraph().setAllowDanglingEdges(false);
+		component.getGraph().alignCells("center");
+		
+		//component.getGraph().setCellStyles(mxConstants.STYLE_STROKECOLOR,"red");
+		
+		//component.getGraph().setStylesheet(stylesheet);
+		
+		//set vertices color
+		//component.getGraph().setCellStyles(mxConstants.STYLE_FILLCOLOR, "red");
 		
 		getContentPane().add(component);
 		resize(DEFAULT_SIZE);
@@ -64,13 +123,15 @@ public class JGraphXAdapterView extends JApplet {
 		mxCircleLayout layout = new mxCircleLayout(jgxAdapter);
 
 		// center the circle
-		int radius = 100;
-		layout.setX0((DEFAULT_SIZE.width / 2.0) - radius);
-		layout.setY0((DEFAULT_SIZE.height / 2.0) - radius);
+		int radius = 170;
+		layout.setX0((DEFAULT_SIZE.width / 1.5) - radius);
+		layout.setY0((DEFAULT_SIZE.height / 1.5) - radius);
 		layout.setRadius(radius);
 		layout.setMoveCircle(true);
 
+		
 		layout.execute(jgxAdapter.getDefaultParent());
 		// that's all there is to it!...
 	}
+	
 }
