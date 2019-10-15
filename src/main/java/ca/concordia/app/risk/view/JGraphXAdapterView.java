1777afd14d1c7740e5cd804035bee32bdf2ea6f1
Nasim Adabi <nasim.adabi@gmail.com>
2019-10-15 17:11:53 -0400

    some refactoring on showmap

--- src/main/java/ca/concordia/app/risk/view/JGraphXAdapterView.java
+++ src/main/java/ca/concordia/app/risk/view/JGraphXAdapterView.java
@@ -11,7 +11,6 @@ import org.jgrapht.ext.JGraphXAdapter;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.DefaultListenableGraph;
 
-//@example:full:begin
 import com.mxgraph.layout.mxCircleLayout;
 import com.mxgraph.model.mxCell;
 import com.mxgraph.model.mxGeometry;
@@ -27,103 +26,57 @@ import ca.concordia.app.risk.model.xmlbeans.CountryModel;
 import ca.concordia.app.risk.model.xmlbeans.PlayerModel;
 
 /**
- * A demo applet that shows how to use JGraphX to visualize JGraphT graphs.
- * Applet based on JGraphAdapterDemo.
+ * Show map graph
  *
  */
 public class JGraphXAdapterView extends JApplet {
+	
 	private static final long serialVersionUID = 2202072534703043194L;
-
 	private static final Dimension DEFAULT_SIZE = new Dimension(550, 420);
-
 	private JGraphXAdapter<String, DefaultEdge> jgxAdapter;
 	
-//	@Autowired
-//	private static GameBusinessDelegate gameBusinessDelegate;
 
 	/**
-	 * An alternative starting point for this demo, to also allow running this
-	 * applet as an application.
 	 *
 	 * @param args command line arguments
 	 */
 	public static void main(String[] args) {
 		JGraphXAdapterView applet = new JGraphXAdapterView();
-		
-//		boolean isMapValid = gameBusinessDelegate.validateMap("All");
-//		try {
-			applet.init();
+		applet.init();
 			
-			JFrame frame = new JFrame();
-			frame.getContentPane().add(applet);
-			frame.setTitle(".:: RiskGame - Show Current Game Map");
-			frame.pack();
-			frame.setVisible(true);
-			//frame.setLocationByPlatform(true);
-			
-			//Size of the frame
-			frame.setPreferredSize(DEFAULT_SIZE);
-			
-			//Shows window in the middle of the screen
-			frame.setLocationRelativeTo(null);
-			
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//		}catch(RiskGameRuntimeException riskGameRuntimeException) {
-//			//System.out.println("No visual view! Graph is not connected!");
-//			throw new RiskGameRuntimeException("No visual view! Graph is not connected!");
-//		}
+		JFrame frame = new JFrame();
+		frame.getContentPane().add(applet);
+		frame.setTitle(".:: RiskGame - Show Current Game Map");
+		frame.pack();
+		frame.setVisible(true);
+		frame.setPreferredSize(DEFAULT_SIZE);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 	}
-	
-	
-	
+
 	@Override
 	public void init() {
 		ListenableGraph<String, DefaultEdge> graph = new DefaultListenableGraph<>(RunningGame.getInstance().getGraph());
-		
-		// create a visualization using JGraph, via an adapter
 		jgxAdapter = new JGraphXAdapter<>(graph);
-
+		
 		jgxAdapter.selectAll();
         Object[] cells = jgxAdapter.getSelectionCells();
-        
-
         jgxAdapter.clearSelection(); 
-        
 		jgxAdapter.setLabelsVisible(true);
-		
 		jgxAdapter.setAutoSizeCells(true);
 		jgxAdapter.alignCells(mxConstants.ALIGN_CENTER);
 		
-		mxStylesheet stylesheet = jgxAdapter.getStylesheet();
-
-        
-		//Make vertices style
-		stylesheet.getDefaultVertexStyle().put(mxConstants.STYLE_ROUNDED, true);
-		
-		//Coloring the Vertices
-		stylesheet.getDefaultVertexStyle().put(mxConstants.STYLE_FILLCOLOR, Color.YELLOW.getRGB());
-		
-		//Let's do add shadow
-		stylesheet.getDefaultVertexStyle().put(mxConstants.STYLE_GRADIENTCOLOR, "#A9C4EB");
-		stylesheet.getDefaultVertexStyle().put(mxConstants.STYLE_SHADOW, true);
-		
-		
-		//Add styles to the adapter
+		mxStylesheet stylesheet = setGraphStyles(jgxAdapter.getStylesheet());
 		jgxAdapter.setStylesheet(stylesheet);
-		jgxAdapter.getStylesheet().getDefaultEdgeStyle().put(mxConstants.STYLE_DASHED, "1");
-		
 		
 		mxGraphComponent component = new mxGraphComponent(jgxAdapter);
-
-		//Prevents drawing edges from user side
 		component.getGraph().setAllowDanglingEdges(false);
 		component.getGraph().alignCells("center");
 		getContentPane().add(component);
 
 		
-	    // Iterate on vertices
-        for (Object c : cells) {
+	    for (Object c : cells) {
             mxCell cell = (mxCell) c;
             mxGeometry geometry = cell.getGeometry();
             
@@ -157,23 +110,32 @@ public class JGraphXAdapterView extends JApplet {
                 cell.setValue(verticeLabel);
             }
         } 
-		
-		//Setting for vertices
-		stylesheet.getDefaultVertexStyle().put(mxConstants.STYLE_AUTOSIZE, 1);
-		stylesheet.getDefaultVertexStyle().put(mxConstants.STYLE_EDGE, mxConstants.EDGESTYLE_ELBOW);
-		stylesheet.getDefaultVertexStyle().put(mxConstants.STYLE_ALIGN, mxConstants.ALIGN_CENTER);
-		
-		//Make graph indirect
-		stylesheet.getDefaultEdgeStyle().put(mxConstants.STYLE_ENDARROW, mxConstants.NONE);
-		stylesheet.getDefaultEdgeStyle().put(mxConstants.STYLE_STARTARROW, mxConstants.NONE);
-		
-		stylesheet.getDefaultEdgeStyle().put(mxConstants.STYLE_NOLABEL, "1");
 
-		//Positioning
 		mxCircleLayout layout = new mxCircleLayout(jgxAdapter);
-	
 		layout.execute(jgxAdapter.getDefaultParent());
 		
+	}
+	
+	/**
+	 * This method adds some style to the graph
+	 *
+	 * @param styleSheet
+	 * @return styleSheet
+	 */
+	private mxStylesheet setGraphStyles(mxStylesheet styleSheet) {
+		styleSheet.getDefaultVertexStyle().put(mxConstants.STYLE_ROUNDED, true);
+		styleSheet.getDefaultVertexStyle().put(mxConstants.STYLE_FILLCOLOR, Color.YELLOW.getRGB());
+		styleSheet.getDefaultVertexStyle().put(mxConstants.STYLE_GRADIENTCOLOR, "#A9C4EB");
+		styleSheet.getDefaultVertexStyle().put(mxConstants.STYLE_SHADOW, true);
+		styleSheet.getDefaultVertexStyle().put(mxConstants.STYLE_AUTOSIZE, 1);
+		styleSheet.getDefaultVertexStyle().put(mxConstants.STYLE_EDGE, mxConstants.EDGESTYLE_ELBOW);
+		styleSheet.getDefaultVertexStyle().put(mxConstants.STYLE_ALIGN, mxConstants.ALIGN_CENTER);
+		styleSheet.getDefaultEdgeStyle().put(mxConstants.STYLE_ENDARROW, mxConstants.NONE);
+		styleSheet.getDefaultEdgeStyle().put(mxConstants.STYLE_STARTARROW, mxConstants.NONE);
+		styleSheet.getDefaultEdgeStyle().put(mxConstants.STYLE_NOLABEL, "1");
+		styleSheet.getDefaultEdgeStyle().put(mxConstants.STYLE_DASHED, "1");
+		
+		return styleSheet;
 	}
 	
 }
