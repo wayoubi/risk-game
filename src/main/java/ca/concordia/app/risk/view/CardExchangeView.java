235d1ced91d6656cf19d48deaf43d3e416527855
Wasim Alayoubi <wasim.alayoubi@mail.concordia.ca>
2019-11-03 20:28:45 -0500

    worked on cards view

--- src/main/java/ca/concordia/app/risk/view/CardExchangeView.java
+++ src/main/java/ca/concordia/app/risk/view/CardExchangeView.java
@@ -1,12 +1,15 @@
 package ca.concordia.app.risk.view;
 
+import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.util.Observable;
 import java.util.Observer;
 
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
+import javax.swing.JTextArea;
 import javax.swing.border.Border;
 
 import ca.concordia.app.risk.model.cache.Player;
@@ -16,42 +19,44 @@ import ca.concordia.app.risk.model.xmlbeans.PlayerModel;
 
 public class CardExchangeView extends JPanel implements Observer {
 
-	JTabbedPane tabbedPane;
-
 	/**
 	 * 
 	 */
 	public CardExchangeView() {	
-		this.setLayout(new GridLayout(1, 1));
-		tabbedPane = new JTabbedPane();
 		Border border = BorderFactory.createTitledBorder("Cards View");
 		this.setBorder(border);
-		this.add(tabbedPane);
-		
-		Player player = RunningGame.getInstance().getCurrentPlayer();
-		if(player!=null) {
-			PlayerModel playerModel = player.getPlayerModel();
-			if(playerModel!=null) {
-				JPanel jPanel = new JPanel();
-				this.tabbedPane.add(jPanel, playerModel.getName());
-			}
-		}	
+		this.setLayout(new GridLayout(1, 1));
+		build();
 	}
 
 	/**
 	 *
 	 */
 	public void update(Observable observable, Object object) {
-		this.tabbedPane.removeAll();
-		
+		this.removeAll();
+		build();
+	}
+	
+	/**
+	 * build the view
+	 */
+	private void build() {
+		JTabbedPane tabbedPane = new JTabbedPane();
 		Player player = RunningGame.getInstance().getCurrentPlayer();
 		if(player!=null) {
 			PlayerModel playerModel = player.getPlayerModel();
 			if(playerModel!=null) {
 				JPanel jPanel = new JPanel();
-				this.tabbedPane.add(jPanel, playerModel.getName());
+				jPanel.setLayout(new BorderLayout());
+				JScrollPane  jScrollPane = new JScrollPane();
+				JTextArea jTextArea = new JTextArea();
+				jTextArea.setText("Hello");
+				jScrollPane.add(jTextArea);
+				jPanel.add(jScrollPane, BorderLayout.CENTER);
+				tabbedPane.add(jPanel, playerModel.getName());
 			}
-		}	
+		}
+		this.add(tabbedPane);
 	}
 
 }
